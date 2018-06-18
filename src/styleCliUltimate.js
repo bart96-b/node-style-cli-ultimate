dfe69f04485cd819b967e2854ca78188e0cbf068
BART96 <bart96@ro.ru>
2018-06-18 03:47:28 +0300

    .

--- src/styleCliUltimate.js
+++ src/styleCliUltimate.js
@@ -9,13 +9,12 @@ const styleCli = require('bart96-style-cli');
 
 class StyleCliUltimate {
   constructor(opt = {}) {
-    if (typeof opt != 'object') process.exit(console.log(`В класс ${this.constructor.name} можно передавать только object.`));
+    if (typeof opt != 'object') throw new Error(`В класс ${this.constructor.name} можно передавать только object.`);
 
     this.config = Object.assign({
-      replacer: [],
-      debugMode: false,
-      defaultTitle: 'error',
-      defaultColor: {
+      replacer  : [],
+      debugMode : false,
+      typeColor : {
         'Null'  : 'white',
 
         'error' : 'red',
@@ -29,32 +28,19 @@ class StyleCliUltimate {
       },
     }, opt);
 
-    this.format = new Format({form:opt.form, UTC:opt.UTC});
+    this.format = new Format({form:this.config.form, UTC:this.config.UTC});
+    this.config.typeArray = Object.keys(this.config.typeColor);
 
-    // this.config = {
-    //   replacer: opt.replacer || [],
-    //   debugMode: opt.debugMode || false,
-    //   defaultTitle: opt.defaultTitle || 'error',
-    //   defaultColor: opt.defaultColor || {
-    //     'Null'  : 'white',
-    //
-    //     'error' : 'red',
-    //     'warn'	: 'yellow',
-    //     'info'	: 'blue',
-    //
-    //     'cmd'		: 'magenta',
-    //     'bot'		: 'cyan',
-    //     'user'  : 'cyan',
-    //     'memory': 'green',
-    //   }
-    // };
-
-    // console.log(styleCli.red(this.format.date()));
+    this.config.typeArray.forEach(typeName => {
+      Object.defineProperty(this, typeName, {
+        value: (...args) => this.log(...args, this.config.typeArray.indexOf(typeName))
+      });
+    });
   }
 
 
   _logString(msg, value) {
-    msg.text += value;
+    msg.text += ' '+ value;
   }
 
   _logNumber(msg, value) {
@@ -63,86 +49,62 @@ class StyleCliUltimate {
   }
 
   _logArray(msg, value) {
-    msg.text += value;
+    if (value[0].stack) msg.text += ' '+ (this.config.debugMode ? value[0].stack : value[1]);
+    else msg.text += ` [${value.toString()}]`;
   }
 
   _logObject(msg, value) {
-    msg.text += value;
+    if (value.stack && value.name && value.message) msg.text = this.config.debugMode ? value.stack : `${msg.text?' \u27A4':''} ${value.name}: ${value.message}`;
+    // else if (el.author) {user = el.author.tag.replace(/#(\d{4})$/, ' $1'); type=6}
+  }
+
+  _logBoolean(msg, value) {
+    msg.text += ' '+ value.toString();
   }
 
   _logFunction(msg, value) {
-    this.log('Данная версия логгера не поддерживает этот тип данных.');
+    this.log(`Данная версия логгера не поддерживает тип данных "${{}.toString.call(value).slice(8, -1)}".`, 2);
+  }
+
+
+  _msgReplacer(msg) {
+    this.config.replacer.forEach(rep => msg.text.replace(rep[0], rep[1]));
+
+    msg.text
+      .replace(/(?:Warn|Error)/g, str => styleCli[str == 'Warn' ? 'yellow' : 'red'](str))
+      .replace(/(?:✖|✔)/g, str => styleCli[str == '✔' ? 'green' : 'red'](str));
+  }
+
+  _msgAssembly(msg) {
+    let typeName = this.config.typeArray[msg.type];
+    let color = this.config.typeColor[typeName] || this.config.typeColor['Null'];
+
+    msg.text = styleCli`{${color} > [${this.format.date()}]} [{${color}Bright ${typeName}}]{white \t=>} {blackBright ${msg.text}}`;
+    // (${moduleParent.filename.split(/\/|\\/).pop()})
   }
 
 
   log(...args) {
     let msg = {
       text: '',
-      type: '',
+      type: 1,
       exit: false,
     };
 
-    for(let item of args) {
-      this.['_log'+ {}.toString.call(item).slice(8, -1)](msg, item);
-    }
+    for(let item of args) this['_log'+ {}.toString.call(item).slice(8, -1)](msg, item);
 
-    if (!msg) return;
+    if (!msg.text) return;
 
+    this._msgReplacer(msg);
+    this._msgAssembly(msg);
 
     msg.exit ? process.exit(console.log(msg.text)) : console.log(msg.text);
-  }
-
-
-
-  _getType(type) {
-    if (typeof type == 'number') return Object.keys(this.config.typeColors)[type] || 'NotFound';
-    if (typeof type == 'string') return this.config.typeColors[type] ? type : 'NotFound';
-    return 'Null';
-  }
-
-  log_OFF(moduleParent, args) {
-    let date = this.format.date(new Date(), this.config.formatDate);
-    let type = this.config.typeFormat;
-    let exit = false;
-
-    let msg = '';
-    let user = '';
-    let color = '';
-
-    for(let el of args) {
-      switch (typeof el) {
-        case 'string': msg += ` ${el}`; break;
-        case 'number': type = Math.abs(el); if (el < 0) exit = true; break;
-  			case 'object':
-  				if (Array.isArray(el) && el[0].stack) msg += this.config.debugMode ? el[0].stack : el[1];
-  				else if (el.stack && el.name && el.message) msg += this.config.debugMode ? el.stack : `${msg?' \u27A4':''} ${el.name}: ${el.message}`;
-  				else if (el.author) {user = el.author.tag.replace(/#(\d{4})$/, ' $1'); type=6}
-  				else this.log(module, [`Отсутствует условие для объекта "${el}". Элемент пропущен`, 2]);
-  			break;
-        // case 'function': ; break;
-
-        default: this.log(module, [`Неизвестный формат "${typeof el}" у "${el}"`, 2]);
-      }
-    }
-
-    if (!msg) return;
-
-    user = this._getType(type);
-    color = this.config.typeColors[user] || this.config.typeColors['Null'];
-
-    msg = msg
-      .replace(/\s(?:Warn|Error)/g, a => cli[a == 'Warn' ? 'yellow' : 'red'](' '+a))
-      .replace(/(✖|✔)/ig, a => cli[a == '✔' ? 'green' : 'red'](a))
-      .replace(/[\w\d]{24}\.[\w\d]{6}\.[\w\d-_]{27}/g, '[ClientToken]')
-      .replace('  ', ' ');
-
-    msg = cli`{${color} > ${date}} [{${color}Bright ${user}}]{white \t=>} (${moduleParent.filename.split(/\/|\\/).pop()}){blackBright ${msg}}`;
-
-    exit ? process.exit(console.log(msg)) : console.log(msg);
   }
 
 }
 
 
-// new StyleCliUltimate();
-module.exports = StyleCliUltimate;
+let logger = new StyleCliUltimate();
+logger.user('123', 5, {});
+
+// module.exports = StyleCliUltimate;
